dcbe53cbce77cb9f413898296974d26e722244b7
NiculaCatalin <niculacatalin97@gmail.com>
2017-03-04 09:27:48 +0200

    Delete trilerp.m

--- trilerp.m
+++
@@ -1,6 +0,0 @@
-function value = trilerp(stack, row, col, level)
-    % Folositi interpolarea triliniara pentru a estima intensitatea unei
-    % imagini in pozitia row, col, level.
-
-    %TODO
-end
